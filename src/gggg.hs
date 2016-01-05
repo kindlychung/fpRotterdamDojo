2584eb9f5952f11386da2f9a8bc59dd5108fadf5
Kaiyin Zhong <kindlychung@gmail.com>
2016-01-05 23:34:25 +0100

    update

--- src/gggg.hs
+++ src/gggg.hs
@@ -1,9 +1,9 @@
 module Ggg where
 
 
+import           Data.Char
+import           Data.List
 import qualified Data.List.Split as L
-import Data.List
-import Data.Char
 
 -- See the challenge on reddit: https://www.reddit.com/r/dailyprogrammer/comments/3x3hqa/20151216_challenge_245_intermediate_ggggggg_gggg/
 
